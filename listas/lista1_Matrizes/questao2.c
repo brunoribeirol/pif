7f9b41781c875692f0dd992e2768df27311e0ce8
Bruno Ribeiro <araujoribeiro.bruno@gmail.com>
2023-09-05 23:17:49 -0300

    Lista 1 matrizes resolvida

---
+++ listas/lista1_Matrizes/questao2.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+
+int main() {
+
+    int G, A, M, C;
+    scanf("%d %d %d %d", &G, &A, &M, &C);
+    printf("\n");
+
+    //Matriz GA
+    int matrizGA[G][A];
+    
+    for (int i = 0; i < G; i++)
+    {
+        for (int j = 0; j < A; j++)
+        {
+            scanf("%d", &matrizGA[i][j]);
+        }
+    }
+    printf("\n");
+
+    //MatrizAM
+    int matrizAM[A][M];
+
+    for (int i = 0; i < A; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            scanf("%d", &matrizAM[i][j]);
+        }
+    }
+    printf("\n");
+
+    //MatrizMC
+    int matrizMC[M][C];
+
+    for (int i = 0; i < M; i++)
+    {
+        for (int j = 0; j < C; j++)
+        {
+            scanf("%d", &matrizMC[i][j]);
+        }
+    }
+    printf("\n");
+
+    //Multiplicação matrizes
+        //MatrizGM
+    int matrizGM[G][M]; //[G][A] x [A][M] = [G][M]; coluna da 1 x linha da 2
+
+    for (int i = 0; i < G; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            matrizGM[i][j] = 0;
+            for (int k = 0; k < A ; k++) //porque A????
+            {
+                matrizGM[i][j] += matrizGA[i][k] * matrizAM[k][j];
+            }
+        }
+    }
+
+        //MatrizGC
+    int matrizGC[G][C]; //[G][M] x [M][C] = [G][C]; coluna da 1 x linha da 2
+
+
+    for (int i = 0; i < G; i++)
+    {
+        for (int j = 0; j < C; j++)
+        {   
+            matrizGC[i][j] = 0; 
+            for (int k = 0; k < M; k++) //porque M???
+            {
+                matrizGC[i][j] += matrizGM[i][k] * matrizMC[k][j];
+            }
+        }
+    }
+
+    //Pritando a matriz final ([G][C])
+    for (int i = 0; i < G; i++)
+    {
+        for (int j = 0; j < C; j++)
+        {
+            printf("%d\t", matrizGC[i][j]);
+        }
+        printf("\n");
+    }
+    
+    printf("\n"); 
+    return 0;
+
+}
+//beecrowd 1967
--- listas/lista1_Matrizes/questao3.c
+++ listas/lista1_Matrizes/questao3.c
@@ -1,42 +1,57 @@
 #include <stdio.h>
+#include <math.h>
+
+// Cria uma função para imprimir uma matriz quadrada
+void matrizQuadrada(int N) {
+    int matriz[15][15]; // Valor máximo da matriz
+
+    // Preenche a matriz
+    for (int i = 0; i < N; i++) // Percorre as linhas
+    {
+        for (int j = 0; j < N; j++) //Percorre as colunas
+        {
+            matriz[i][j] = pow(2, i) * pow(2, j); // Eleva ao quadrado o valor de i e j no momento
+        }
+    }
+
+    // Determina o T máximo para formatar a saída
+    int T = 0;
+    int maiorNumero = matriz[N-1][N-1]; // O maior número na matriz, elemento da matriz que está na última linha e coluna
+    while (maiorNumero > 0) {
+        maiorNumero /= 10;
+        T++;
+    }
+
+    // Imprime a matriz formatada
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            printf("%*d", T, matriz[i][j]);
+            if (j < N - 1)
+            {
+                printf(" "); // Adiciona um espaço entre os números na mesma linha
+            }
+        }
+        printf("\n"); // Nova linha após cada linha da matriz
+    }
+}
 
 int main() {
+    int N; // Tamanho da matriz quadrada ([2][2], [3][3]...)
 
-    int N; //matriz quadrada ([2][2], [3][3]...)
-
-    while (1) 
+    while (1)
     {
         scanf("%d", &N);
 
-        if (N == 0) //colocar algo relacionado ao 15?
+        if (N == 0)
         {
             break;
         }
-        
-        int matriz[N][N]; //colocar aqui [15][15]?
-        int T = 1;
-        int valor = 1;
 
-        int inicio = matriz[0][0] = 1;
-
-
-
-        for (int i = 0; i < N; i++)
-        {
-            for (int j = 0; j < N; j++)
-            {
-                printf("%d", inicio);
-
-                
-            }
-        }
-
-           
-
-        printf("\n"); // Imprime uma linha em branco após o array
+        matrizQuadrada(N); // Chama a funcao
+        printf("\n"); // Adiciona uma linha em branco entre matrizes
     }
-
 
     return 0;
 }
-//beecrowd 1557
