000fd58c452267f09acc5733f6ae87cbeec93859
Bruno Ribeiro <araujoribeiro.bruno@gmail.com>
2023-09-14 23:24:12 -0300

    Adicionando questoes

--- aulas/replitPamela/aula2/variaveis_e_operacoes.c
+++ aulas/replitPamela/aula2/variaveis_e_operacoes.c
@@ -13,15 +13,15 @@ int main() {
     modulo = a%b;
     divisao = a/b;
 
-    printf("A soma de a e b é iqual a %d \n", soma);
-    printf("O resto da divisão de a e b é iqual a %d\n", modulo);
-    printf("A divisão de a e b é iqual a %.2f\n", divisao);
+    printf("A soma de a e b é igual a %d \n", soma);
+    printf("O resto da divisão de a e b é igual a %d\n", modulo);
+    printf("A divisão de a e b é igual a %.2f\n", divisao);
 
     float c, d;
     
     scanf("%f %f",&c, &d);
     divisao = c/d;
-    printf("A divisão de c e d é iqual a %.2f\n", divisao);
+    printf("A divisão de c e d é igual a %.2f\n", divisao);
 
     return 0;
 }
---
+++ miniProvas/miniProva1.c
@@ -0,0 +1,37 @@
+/*Pedia para checar se um caracter ch estava presente na string str, se tivesse retornava
+o índice na funcao, caso nao estivesse presente retornava -1*/
+
+//Como eu fiz
+int strch(char *str, char ch) {
+
+    int tamanho = strlen(str); //acho que nao coloquei o tipo int da variavel
+    for (int i = 0; i < tamanho; i++)
+    {
+        if (str[i] == ch)
+        {
+            return i;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+}
+
+//Como era pra ter feito
+int strch(char *str, char ch) {
+    int tamanho = strlen(str);
+    
+    for (int i = 0; i < tamanho; i++) {
+        if (str[i] == ch) {
+            return i; // Retorna a posição quando o caractere é encontrado
+        }
+    }
+    
+    return -1; // Retorna -1 se o caractere não for encontrado
+}
+
+
+
+
+
