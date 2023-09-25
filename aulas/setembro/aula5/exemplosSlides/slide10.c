9e099897b901d9f968f6f031b2b349c675800eae
Bruno Ribeiro <araujoribeiro.bruno@gmail.com>
2023-09-25 18:38:30 -0300

    Lista 3 beecrowd

--- aulas/setembro/aula5/exemplosSlides/slide10.c
+++ aulas/setembro/aula5/exemplosSlides/slide10.c
@@ -1,32 +1,79 @@
+//STRUCT FUNÇÃO
+
 #include <stdio.h>
+#include <stdlib.h>
 
-struct rect makescreen(struct point p1, struct point p2)
+//definindo um ponto
+struct point
 {
-    struct rect temp;
+    int x;
+    int y;
+};
 
-    temp.pt1 = p1;
+//definindo um retangulo
+struct rect 
+{
+    struct point pt1;
+    struct point pt2;
+};
 
-    temp.pt2 = p2;
-
+//funcao para criar um novo ponto
+struct point makepoint(int x, int y)
+{ 
+    struct point temp;
+    temp.x = x;
+    temp.y = y;
     return temp;
-
 }
 
-int area_rect(struct rect retangulo){
-
-int altura,base;
-
-altura = abs(retangulo.pt1.y - retangulo.pt2.y);
-
-base = abs(retangulo.pt1.x - retangulo.pt2.x);
-
-return altura*base;
-
+//funcao para criar um novo retangulo
+struct rect makescreen(struct point p1, struct point p2)
+{ 
+    struct rect temp;
+    temp.pt1 = p1;
+    temp.pt2 = p2;
+    return temp;
 }
 
-int main()
+//area do retangulo
+int area_rect(struct rect retangulo)
+{
+    int altura,base;
+    altura = abs(retangulo.pt1.y - retangulo.pt2.y);
+    base = abs(retangulo.pt1.x - retangulo.pt2.x);
+
+    return altura*base;
+}
+
+int main(void)
 {
 
+  int a, b, area; 
+  struct point pt_1, pt_2;
+  //struct point *pp; //ponteiro do tipo struct
+  struct rect retangulo;
 
-    return 0;
+  printf("Digite as coordenadas do primeiro ponto\n");
+  scanf("%d %d", &a, &b);
+  pt_1 = makepoint(a, b);
+
+  printf("As coordenadas do seu novo ponto sao (%d,%d)\n",pt_1.x, pt_1.y);
+
+  //pp= &pt;
+  //printf("As coordenadas do seu novo ponteiro sao (%d,%d)\n", pp->x, pp->y);
+
+  printf("Digite as coordenadas do seu segundo ponto\n");
+  scanf("%d %d", &a, &b);
+  pt_2 = makepoint(a,b);
+
+  printf("As coordenadas do seu novo ponto sao (%d,%d)\n", pt_2.x, pt_2.y);
+  
+  retangulo = makescreen(pt_1, pt_2);
+  area = area_rect(retangulo);
+
+  printf("Area do retangulo construido: %d\n", area);
+  //pp = &pt_2;
+ //printf("As coordenadas do seu segundo ponto sao (%d,%d)\n", pp->x, pp->y);
+
+  return 0;
 }
--- listas/lista3_StringsStructs/questao1.c
+++ listas/lista3_StringsStructs/questao1.c
@@ -1,29 +1,46 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+
 
 int main()
 {
 
-    int N; //numero de testes
-    scanf("%d", &N);
+    int testCases; //number of testCases
+    scanf("%d", &testCases);
 
-    char str;
+    getchar(); //did not undestand how does it work yet
 
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < testCases; i++)
     {
-        fgets(str, N, stdin);
+        char phrase[200]; //199 + \0
+        int countLetters[26] = {0};
+
+        fgets(phrase, sizeof(phrase), stdin); //add comment HERE
+
+        printf("%s", phrase);
+
+        for (int i = 0; i < strlen(phrase); i++)
+        {
+            char ch = tolower(phrase[i]); //converting all the characters to lower case
+            
+            if (isalpha(ch))
+            {
+                /* code */
+            }
+            
+
+
+        }
+        
+
 
     }
     
-    
-
-
-
-
 
     return 0;
 }
 
+//scanf("%[^\n]", phrase);
 
-
-//beecrowd 1255
+//beecrowd1 255
