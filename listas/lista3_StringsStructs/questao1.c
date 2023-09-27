195de8cdb26f4ed42651535db937b936969bc490
Bruno Ribeiro <araujoribeiro.bruno@gmail.com>
2023-09-26 22:28:16 -0300

    Atualizando

--- listas/lista3_StringsStructs/questao1.c
+++ listas/lista3_StringsStructs/questao1.c
@@ -1,46 +1,59 @@
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
-
 
 int main()
 {
-
-    int testCases; //number of testCases
+    int testCases; // Number of testCases
     scanf("%d", &testCases);
 
-    getchar(); //did not undestand how does it work yet
+    getchar(); // Consumes the newline character left in the input buffer after reading testCases
 
     for (int i = 0; i < testCases; i++)
     {
-        char phrase[200]; //199 + \0
-        int countLetters[26] = {0};
+        char phrase[200]; // 199 + /0
 
-        fgets(phrase, sizeof(phrase), stdin); //add comment HERE
+        scanf(" %[^\n]", phrase); // Read a line of text (excluding leading whitespace)
 
-        printf("%s", phrase);
+        int countLetters[26] = {0}; // An array to store the count of each letter
 
         for (int i = 0; i < strlen(phrase); i++)
         {
-            char ch = tolower(phrase[i]); //converting all the characters to lower case
-            
-            if (isalpha(ch))
+            char ch = phrase[i];
+
+            // Convert characters to lowercase (assuming ASCII values)
+            if (ch >= 'A' && ch <= 'Z')
             {
-                /* code */
+                ch += 32;
             }
-            
 
-
+            // Check if the character is a letter (assuming ASCII values)
+            if (ch >= 'a' && ch <= 'z')
+            {
+                countLetters[ch - 'a']++; // Increment the count for the specific letter
+            }
         }
-        
 
+        int maxCount = 0;
 
+        for (int i = 0; i < 26; i++)
+        {
+            if (countLetters[i] > maxCount)
+            {
+                maxCount = countLetters[i]; // Find the maximum letter count
+            }
+        }
+
+        for (int i = 0; i < 26; i++)
+        {
+            if (countLetters[i] == maxCount)
+            {
+                printf("%c", 'a' + i); // Print letters with the maximum count
+            }
+        }
+        printf("\n");
     }
-    
 
     return 0;
 }
 
-//scanf("%[^\n]", phrase);
-
-//beecrowd1 255
+//beecrowd 1255
--- listas/lista3_StringsStructs/questao3.c
+++ listas/lista3_StringsStructs/questao3.c
@@ -1,1 +1,55 @@
-//beecrowd 1554
+#include <stdio.h>
+#include <math.h>
+
+struct balls
+{
+    int x;
+    int y;
+};
+
+int main()
+{
+
+    int testCases; // Read the number of test cases
+    scanf("%d", &testCases);
+
+    while (testCases--)
+    {
+        int ballsNumbers; // Read the number of balls in the current test case
+        scanf("%d", &ballsNumbers);
+
+        // Create a struct to represent the white ball and an array for other balls
+        struct balls whiteBall;
+        struct balls otherBalls[ballsNumbers];
+
+        scanf("%d %d", &whiteBall.x, &whiteBall.y); // Read the coordinates of the white ball
+
+        int rightBall = 0;
+        float minDistance = 0;
+
+        // Loop through all the balls
+        for (int i = 0; i < ballsNumbers; i++)
+        {
+            // Read the coordinates of each ball
+            scanf("%d %d", &otherBalls[i].x, &otherBalls[i].y);
+
+            // Calculate the distance between the white ball and the current ball
+            float distance = sqrt(pow(whiteBall.x - otherBalls[i].x, 2) + pow(whiteBall.y - otherBalls[i].y, 2));
+
+            // Check if this is the first ball or if it's closer than the previous closest ball
+            if (i == 0 || distance < minDistance)
+            {
+                // Update the minimum distance and the index of the right ball
+                minDistance = distance;
+                rightBall = i + 1; // Adding 1 to the index because ball numbering starts from 1
+            }
+        }
+
+        // Print the index of the right ball for this test case
+        printf("%d\n", rightBall);
+    }
+
+    return 0;
+}
+
+//beecrowd 1255
