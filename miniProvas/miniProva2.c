63bf37628c6cec593179dab7685a1b0b06befcd4
Bruno Ribeiro <araujoribeiro.bruno@gmail.com>
2023-09-28 16:29:36 -0300

    Changed test 2

--- miniProvas/miniProva2.c
+++ miniProvas/miniProva2.c
@@ -1,41 +1,5 @@
-//COMO EU FIZ
+// Create 2 variables and insert the following
 
-#include <stdio.h>
-
-union value {
-  int reais;
-  int dollars;
-};
-
-struct books {
-  char title[50];
-  char author[50];
-  int id;
-  union value price;
-};
-
-int main()
-{
-
-    struct books Book1;
-    fgets(Book1.title, 50, stdin); //scanf("%[ˆ\n]", Book1.title)
-    fgets(Book1.author, 50, stdin);
-    scanf("%d", Book1.id);
-    scanf("%d", price.Book1.reais);
-    scanf("%d", price.Book1.dollars);
-
-    struct books Book2;
-    fgets(Book2.title, 50, stdin);
-    fgets(Book2.author, 50, stdin);
-    scanf("%d", Book2.id);
-    scanf("%d", price.Book2.reais);
-    scanf("%d", price.Book2.dollars);
-
-  return 0;
-}
-
-
-//COMO ERA PRA TER FEITO
 #include <stdio.h>
 
 union value
@@ -59,7 +23,7 @@ int main()
     fgets(Book1.title, 50, stdin); //scanf("%[ˆ\n]", Book1.title)
     fgets(Book1.author, 50, stdin);
     scanf("%d", &Book1.id);
-    scanf("%d", &Book1.price.reais);
+    scanf("%d", &Book1.price.reais); // i wrote &price.Book1.reais
     scanf("%d", &Book1.price.dollars);
 
     struct books Book2;
